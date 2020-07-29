805d7f8dee4b03df1cc29e8494b7bba16383d63f
Rohit Raj Jalheria <rjalheira@gmail.com>
2020-07-29 23:33:59 +0530

    Add files via upload

---
+++ NLP_basics/4_spellchecker.py
@@ -0,0 +1,22 @@
+"""
+Created on Wed Jul 29 21:21:22 2020
+
+@author: jhhalls
+
+"""
+
+
+from spellchecker import SpellChecker
+
+spell = SpellChecker()
+
+#find the woeds that may be mispelled
+misspell = spell.unknown(['let', 'us', 'walk', 'on', 'teh'])
+
+for word in misspell:
+#    get the one most likely answer
+    print(spell.correction(word))
+    
+#    get the list of likely words
+    print(spell.candidates(word))
+    
